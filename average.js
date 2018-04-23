f423beefe2329ffa648eb94bd53862620552bc2a
ewal2 <38157680+ewal2@users.noreply.github.com>
2018-04-24 01:25:24 +0200

    Create average.js

---
+++ average.js
@@ -0,0 +1,16 @@
+function average(arr){
+var sum = 0;
+for( var i = 0; i < arr.length; i++){
+    sum += arr[i];
+}
+var avg = sum/arr.length;
+return Math.round(avg);
+}
+
+console.log("Average score for environmental science");
+var scores = [90,98,89,100,100,86,94];
+console.log(average(scores));
+
+console.log("Average score for organic chemisty");
+var scores2 = [40,65,77,88,80,54,73,63,95,49];
+console.log(average(scores2));
