459893f9482e7de93f371a2c142a04e498304ca3
ewal2 <38157680+ewal2@users.noreply.github.com>
2018-04-25 01:13:00 +0200

    Second example

--- app-start.js
+++ app-start.js
@@ -33,8 +33,52 @@ app.get("*", function(req, res){
 });
 
 
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");
+});
 
+-------------------------------------------------------
+    var express = require("express");
+var app = express();
+
+app.get("/speak/pig", function(req, res){
+    res.send("The pig says 'Oinnk'");
+});
+
+app.get("/", function(req, res){
+    res.send("Hi there, welcom to my assigment!");
+});
+
+app.get("/speak/cow", function(req, res){
+    res.send("The cow says 'Moo'");
+});
+
+app.get("/speak/dog", function(req, res){
+    res.send("The dog says 'Woof Woof!'");
+});
+
+app.get("/repeat/hello/3", function(req, res){
+    res.send("hello hello hello");
+});
+
+app.get("/repeat/hello/5", function(req, res){
+    res.send("hello hello hello hello hello");
+});
+
+app.get("/repeat/hello/5", function(req, res){
+    res.send("hello hello hello hello hello");
+});
+
+app.get("/repeat/blah/2", function(req, res){
+    res.send("blah blah");
+});
+
+
+app.get("*", function(req, res){
+    res.send("Sorry, page not found...What are you doing with your life?");   
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 });
+
