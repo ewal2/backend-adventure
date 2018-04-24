38f131882859e30545cc450454f3244d99889ba6
ewal2 <38157680+ewal2@users.noreply.github.com>
2018-04-24 04:26:52 +0200

    Create start app.js

---
+++ app-start.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there"
+
+app.get("/", function(req, res){
+    res.send("Hi there");
+});
+
+app.get("/bye", function(req, res){
+    res.send("Goodbye");
+});
+
+app.get("/dog", function(req, res){
+    res.send("MEOW");
+});
