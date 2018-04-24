3d029dd2b178ce9dbf8788a6afd5608127176457
ewal2 <38157680+ewal2@users.noreply.github.com>
2018-04-24 05:57:17 +0200

    Update app.js

--- app-start.js
+++ app-start.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 
+
+
 // "/" => "Hi there"
 
 app.get("/", function(req, res){
@@ -14,4 +16,25 @@ app.get("/bye", function(req, res){
 app.get("/dog", function(req, res){
     console.log("Someone made a request to /dog!");
     res.send("MEOW");
+});
+
+app.get("/r/:subredditName", function(req, res){
+    console.log(req.params);
+   res.send("Welcome to Subreddit!"); 
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+      console.log(req.params);
+   res.send("Welcome to the comments page!"); 
+});
+
+app.get("*", function(req, res){
+    res.send("You are a star!");   
+});
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");
 });
